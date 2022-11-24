47961a664bb1e272a5f39266cfa8bc146d23757c
Spyke27 <rodrigoeufrasio2@gmail.com>
2022-11-24 12:00:01 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './home/Home'
+import Home from './paginas/home/Home'
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
 
 function App() {
   return (
    <>
+    <Navbar />
     <Home />
+    <Footer/>
    </>
   );
 }
---
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,59 @@
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar variant="dense" className="bar">
+          <Box style={{ cursor: "pointer" }}>
+            <Typography variant="h5" color="inherit">
+              BlogPessoal
+            </Typography>
+          </Box>
+
+          <Box display="flex" justifyContent="start">
+            <Box className="btn-home pointer" mx={1}>
+              <Typography variant="h6" color="inherit">
+                home
+              </Typography>
+            </Box>
+            
+            <Box className="pointer" mx={1}>
+              <Typography variant="h6" color="inherit">
+                postagens
+              </Typography>
+            </Box>
+
+            <div className='separador'></div>
+
+            <Box className="pointer" mx={1}>
+              <Typography variant="h6" color="inherit">
+                temas
+              </Typography>
+            </Box>
+
+            <div className='separador'></div>
+
+            <Box className="pointer" mx={1}>
+              <Typography variant="h6" color="inherit">
+                cadastrar tema
+              </Typography>
+            </Box>
+            
+            <div className='separador'></div>
+
+            <Box className="pointer" mx={1}>
+              <Typography variant="h6" color="inherit">
+                logout
+              </Typography>
+            </Box>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
+export default Navbar;
